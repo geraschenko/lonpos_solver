1da19080a8e609f5e3772513278675da1b5ef0d4
Anton Geraschenko <geraschenko@gmail.com>
2022-02-01 22:19:22 -0800

    Bump the version to 0.1. Likely the higest it will ever get.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='lonpos_solver',
-    version='0.0',
+    version='0.1',
     description='A Lonpos solver.',
     author='Anton Geraschenko',
     author_email='geraschenko@gmail.com',
